b24cce98a651d6167f062a1f5ea21c4d0fb8c81b
jw0111 <59572441+jw0111@users.noreply.github.com>
2021-05-18 03:49:10 +0900

    대화창 생성

---
+++ Assets/Scripts/DialougeSystem.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialougeSystem : MonoBehaviour
+{
+    public Text txtName;
+    public Text txtSentence;
+
+    Queue<string> sentences = new Queue<string>();
+
+    public Animator anim;
+
+    public void Begin(Dialouge info)
+    {
+        anim.SetBool("isOpen", true);
+        sentences.Clear();
+        
+        txtName.text = info.name;
+
+        foreach(var sentence in info.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        Next();
+    }
+
+    public void Next()
+    {
+        if(sentences.Count == 0)
+        {
+            End();
+            return;
+        }
+        //txtSentence.text = sentences.Dequeue();
+        txtSentence.text = string.Empty;
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentences.Dequeue()));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        foreach(var letter in sentence)
+        {
+            txtSentence.text += letter;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    public void End()
+    {
+        txtSentence.text = string.Empty;
+        anim.SetBool("isOpen", false);
+    }
+}
---
+++ Assets/Scripts/DialougeTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialougeTrigger : MonoBehaviour
+{
+    public Dialouge info;
+
+    public void Trigger()
+    {
+        var system = FindObjectOfType<DialougeSystem>();
+        system.Begin(info);
+    }
+}
